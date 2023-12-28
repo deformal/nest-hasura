98c9cbee5996b11a6ad722ac1aa45b2203ea17ac
deformal <drake9211@gmail.com>
2023-12-28 15:14:18 +0530

    setup hasura migrations

---
+++ hasura/migrations/default/1703616690783_squashed/down.sql
@@ -0,0 +1,6 @@
+
+DELETE FROM "products"."product_type" WHERE "value" = 'KITCHEN_USE';
+
+DELETE FROM "products"."product_type" WHERE "value" = 'GENERAL_USE';
+
+DROP TABLE "products"."product_type";
