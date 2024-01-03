276de21693f1a0e3f9aafbe5cd5aa8e0b99b420e
saurabh <drake9211@gmail.com>
2024-01-04 04:28:17 +0530

    v2.2.5

--- runLocal.sh
+++ runLocal.sh
@@ -4,6 +4,7 @@ if docker ps -q 2>/dev/null | grep -q .; then
     containers_running=true
     echo "Are there running containers? $containers_running"
     docker kill $(docker ps -q)
+    minikube start
 else
     containers_running=false
     echo "Are there running containers? $containers_running"
